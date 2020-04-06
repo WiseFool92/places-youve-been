f6517f06cdf9d2971dc6329cd4b7c838fa568fdc
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-06 16:18:06 -0700

    scripts.js UI takes inputs and displays & hides with a click

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,7 @@ Place.prototype.addlandmark = function(landmark){
 }
 
 var locationArray = [];
-// TEST 
-// gather inputs from user
-//debugger;
-// run prototype method
 
-// Frontend UI
 $(document).ready(function() {
 
   $("#location-form").submit(function(event) {
@@ -31,65 +26,26 @@ $(document).ready(function() {
     locationArray.push(newPlace);
 
     $("#output").empty();
+    
     for(var i = 0; i < locationArray.length; i++) {
-      $("#output").append("<div id=\"click-link\">");
-      $("#output").append("<h2>" + locationArray[i].location + "</h2>");
-      $("#output").append("<ul class=\"location-info\">");
-      $("#output").append("<li>" + locationArray[i].description + "</li>");
-      $("#output").append("<li>" + locationArray[i].season + "</li>");
-      $("#output").append("<li><strong>Landmarks</strong></li>");
-      for(var j = 0; j < locationArray[i].landmarks.length; j++){ // for landmarks array to iterate through
-        $("#output").append("<li>" + locationArray[i].landmarks[j] + "</li>");        
+      var result = `
+      <div id="click-link${i}">
+        <h2>${locationArray[i].location}</h2>
+        <ul id="unorderedList${i}" class="location-info">
+          <li>${locationArray[i].description}</li>
+          <li>${locationArray[i].season}</li>
+          <li><strong>Landmarks</strong></li>
+        </ul>
+      </div>
+      `;
+      $("#output").append(result);
+      for(var j = 0; j < locationArray[i].landmarks.length; j++) {
+        $(`#unorderedList${i}`).append(`<li>${locationArray[i].landmarks[j]}</li>`);
       }
-      $("#output").append("</ul>");
-      $("#output").append("</div>");
+      $(`#click-link${i}`).click(function() {
+        $(this).children("ul").toggleClass("location-info");
+      });
     }
-
     event.preventDefault();
   });
-
-  //string literal example
-  for() {
-    $("#output").append(`
-    <div id="click-link">
-      <h2>${locationArray[i].location}</h2>
-      <ul id="unorderedList${i}" class="location-info">
-        <li>${locationArray[i].description}</li>
-        <li>${locationArray[i].season}</li>
-        <li><strong>${Landmarks[i]}</strong></li>
-      </ul>
-    </div>
-    `);
-
-    for(var j = 0; j < locationArray[i].landmarks.length; j++) {
-      $(`#unorderedList${i}`).append(`<li>${locationArray[i].landmarks[j]}</li>`);
-    }
-  }
-
-  
-
-  $("#click-link").click(function() {
-    $(this).children("ul").toggleClass("location-info");
-  });
-
-// A second method we were testing to perform the same task as above...it didn't work
-  // $("#location-form").submit(function(event) {
-  //   event.preventDefault();
-  //   var inputLocation = $("#location").val();
-  //   var inputLandmarks = [];
-  //   inputLandmarks.push($("#landmark1").val());
-  //   inputLandmarks.push($("#landmark2").val());
-  //   inputLandmarks.push($("#landmark3").val());
-  //   var inputSeason = $("#season").val();
-  //   var inputDescription = $("#description").val();
-  //   var newPlace = new Place(inputLocation, inputLandmarks, inputSeason, inputDescription);
-
-  //   $("output").append("<li class = 'outputLocation'><h1>" + newPlace + 
-  //   inputLocation + "</h1><br><p class = 'outputDescription'>" + newPlace + 
-  //   inputSeason + "" + newPlace + "</p></li>");
-  //   $(".outputLocation").click(function() {
-  //     $(this).find(".outputDescription").toggle();
-  //   })
-  // })
-  
-})
+});
