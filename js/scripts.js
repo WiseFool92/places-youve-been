5a3ab75ca6a2f1b6226dc9a913dba05c85a5acc5
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-06 14:55:21 -0700

    test alternate methods for collecting and displaying information in js

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,70 @@ function Place(location, landmarks, season, description) {
   this.landmarks = landmarks;
   this.season = season;
   this.description = description;
-  //this.name = name;
 }
 
 Place.prototype.addlandmark = function(landmark){
   this.landmarks.push(landmark);
 }
 
-var locationArray = []
+var locationArray = [];
 // TEST 
 // gather inputs from user
-var inputLocation = "Seattle";
-var inputLandmarks = ["Space Needle", "Pike Place", "Gas Works Park", "Lake Washington"];
-var inputSeason = "Summer";
-var inputDescription = "Seattle is a seaport city on the West Coast of the United States. It is the seat of King County, Washington.";
-var newPlace = new Place(inputLocation, inputLandmarks, inputSeason, inputDescription);
 //debugger;
 // run prototype method
 
 // Frontend UI
 $(document).ready(function() {
 
-  event.preventDefault();
-  $("#output").text();
+  $("#location-form").submit(function(event) {
+    var inputLocation = $("#location").val();
+    var inputLandmarks = [];
+    inputLandmarks.push($("#landmark1").val());
+    inputLandmarks.push($("#landmark2").val());
+    inputLandmarks.push($("#landmark3").val());
+    var inputSeason = $("#season").val();
+    var inputDescription = $("#description").val();
+    var newPlace = new Place(inputLocation, inputLandmarks, inputSeason, inputDescription);
+    locationArray.push(newPlace);
+
+    $("#output").text("");
+    for(var i = 0; i < locationArray.length; i++) {
+      $("#output").append ("<div id=\"click-link\">");
+      $("#output").append ("<h2>" + locationArray[i].location + "</h2>");
+      $("#output").append ("<ul class=\"location-info\">");
+      $("#output").append("<li>" + locationArray[i].description + "</li>");
+      $("#output").append("<li>" + locationArray[i].season + "</li>");
+      $("#output").append("<li><strong>Landmarks</strong></li>");
+      for(var j = 0; j < locationArray[i].landmarks.length; j++){ // for landmarks array to iterate through
+        $("#output").append("<li>" + locationArray[i].landmarks[j] + "</li>");        
+      }
+      $("#output").append("</ul>");
+      $("#output").append("</div>");
+    }
+
+    event.preventDefault();
+  });
+
+  $("#click-link").click(function() {
+    $(this).children("ul").toggleClass("location-info");
+  });
+
+// A second method we were testing to perform the same task as above
+  $("#location-form").submit(function(event) {
+    event.preventDefault();
+    var inputLocation = $("#location").val();
+    var inputLandmarks = [];
+    inputLandmarks.push($("#landmark1").val());
+    inputLandmarks.push($("#landmark2").val());
+    inputLandmarks.push($("#landmark3").val());
+    var inputSeason = $("#season").val();
+    var inputDescription = $("#description").val();
+    var newPlace = new Place(inputLocation, inputLandmarks, inputSeason, inputDescription);
+
+    $("output").append("<li class = 'outputLocation'><h1>" + newPlace + inputLocation + "</h1>" + newPlace + inputSeason + "" + newPlace + "</li>");
+    $("outputLocation").click(function() {
+      $(this).find("outputDescription").toggle();
+    })
+  })
+  
 })
